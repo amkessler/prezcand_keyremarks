bef5bb085d04844683fe23817a1bfa5db799294a
amkessler <kessler.aaron@gmail.com>
2019-06-20 16:14:00 -0400

    rename subject_1 as subcategories

--- 00_gsheets_load_data.R
+++ 00_gsheets_load_data.R
@@ -36,47 +36,47 @@ keyremarks$date <- mdy(keyremarks$date)
 #create top level categories
 keyremarks <- keyremarks %>% 
   mutate(
-    subject_1 = str_to_lower(subject_1),
+    subcategories = str_to_lower(subject_1),
     category = case_when(
-      str_detect(subject_1, "abortion") ~ "abortion",
-      str_detect(subject_1, "roe vs. wade") ~ "abortion",
-      str_detect(subject_1, "roe v. wade") ~ "abortion",
-      str_detect(subject_1, "health care") ~ "healthcare",
-      str_detect(subject_1, "healthcare") ~ "healthcare",
-      str_detect(subject_1, "obamacare") ~ "healthcare",
-      str_detect(subject_1, "medicare") ~ "healthcare",
-      str_detect(subject_1, "climate") ~ "climate",
-      str_detect(subject_1, "tuition") ~ "tuition",
-      str_detect(subject_1, "student debt") ~ "tuition",
-      str_detect(subject_1, "college affordability") ~ "tuition",
-      str_detect(subject_1, "guns") ~ "guns",
-      str_detect(subject_1, "gun ") ~ "guns",
-      str_detect(subject_1, "immigration") ~ "immigration",
-      str_detect(subject_1, "immigrant") ~ "immigration",
-      str_detect(subject_1, "migrant") ~ "immigration",
-      str_detect(subject_1, "child separation") ~ "immigration",
-      str_detect(subject_1, "border") ~ "immigration",
-      str_detect(subject_1, "criminal justice") ~ "criminal justice",
-      str_detect(subject_1, "cash bail") ~ "criminal justice",
-      str_detect(subject_1, "racial justice") ~ "criminal justice",
-      str_detect(subject_1, "economy") ~ "economy",
-      str_detect(subject_1, "economic") ~ "economy",
-      str_detect(subject_1, "minimum wage") ~ "economy",
-      str_detect(subject_1, "living wage") ~ "economy",
-      str_detect(subject_1, "wages") ~ "economy",
-      str_detect(subject_1, "income") ~ "economy"
+      str_detect(subcategories, "abortion") ~ "abortion",
+      str_detect(subcategories, "roe vs. wade") ~ "abortion",
+      str_detect(subcategories, "roe v. wade") ~ "abortion",
+      str_detect(subcategories, "health care") ~ "healthcare",
+      str_detect(subcategories, "healthcare") ~ "healthcare",
+      str_detect(subcategories, "obamacare") ~ "healthcare",
+      str_detect(subcategories, "medicare") ~ "healthcare",
+      str_detect(subcategories, "climate") ~ "climate",
+      str_detect(subcategories, "tuition") ~ "tuition",
+      str_detect(subcategories, "student debt") ~ "tuition",
+      str_detect(subcategories, "college affordability") ~ "tuition",
+      str_detect(subcategories, "guns") ~ "guns",
+      str_detect(subcategories, "gun ") ~ "guns",
+      str_detect(subcategories, "immigration") ~ "immigration",
+      str_detect(subcategories, "immigrant") ~ "immigration",
+      str_detect(subcategories, "migrant") ~ "immigration",
+      str_detect(subcategories, "child separation") ~ "immigration",
+      str_detect(subcategories, "border") ~ "immigration",
+      str_detect(subcategories, "criminal justice") ~ "criminal justice",
+      str_detect(subcategories, "cash bail") ~ "criminal justice",
+      str_detect(subcategories, "racial justice") ~ "criminal justice",
+      str_detect(subcategories, "economy") ~ "economy",
+      str_detect(subcategories, "economic") ~ "economy",
+      str_detect(subcategories, "minimum wage") ~ "economy",
+      str_detect(subcategories, "living wage") ~ "economy",
+      str_detect(subcategories, "wages") ~ "economy",
+      str_detect(subcategories, "income") ~ "economy"
     )
   )
 
 
 #case of categories
 keyremarks$category <- str_to_title(keyremarks$category)
-keyremarks$subject_1 <- str_to_title(keyremarks$subject_1)
+keyremarks$subcategories <- str_to_title(keyremarks$subcategories)
 
 
 #format as factors for DT use
 keyremarks$category <- as.factor(keyremarks$category)
-keyremarks$subject_1 <- as.factor(keyremarks$subject_1)
+keyremarks$subcategories <- as.factor(keyremarks$subcategories)
 keyremarks$candidate <- as.factor(keyremarks$candidate)
 keyremarks$venue <- as.factor(keyremarks$venue)
 keyremarks$state <- as.factor(keyremarks$state)
