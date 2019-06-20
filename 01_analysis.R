af2f64d6cf05d133736c1f9e27f25579f9cb0d75
amkessler <kessler.aaron@gmail.com>
2019-06-20 16:21:19 -0400

    initial commit

---
+++ 01_analysis.R
@@ -0,0 +1,36 @@
+library(tidyverse)
+library(lubridate)
+library(janitor)
+library(glue)
+
+keyremarks <- readRDS("keyremarks_forMP.rds")
+
+remarks_sincemidterms <- keyremarks %>% 
+  filter(date > "2018-11-06") %>% 
+  select(candidate,
+         date,
+         venue,
+         city,
+         state,
+         category,
+         subcategories,
+         key_sound,
+         other_sound,
+         source_link
+  ) %>% 
+  arrange(desc(date))
+
+
+
+# some aggregate counts ####
+
+remarks_sincemidterms %>% 
+  filter(!is.na(category)) %>% 
+  count(category) %>% 
+  arrange(desc(n))
+
+
+remarks_sincemidterms %>% 
+  filter(!is.na(category)) %>% 
+  count(candidate, category) %>% 
+  arrange(candidate, desc(n))
